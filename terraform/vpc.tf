0c2c557694cee45d019aafbe711b0daa381f0cb7
Chirrag Sapra <chirragsapra@ip-192-168-1-21.ap-south-1.compute.internal>
2026-04-09 00:10:13 +0530

    added NAT and pvt rt

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -77,3 +77,40 @@ resource "aws_route_table_association" "public_subnet_2_assoc" {
   subnet_id      = aws_subnet.public_subnet_2.id
   route_table_id = aws_route_table.public_rt.id
 }
+
+
+### NAT
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet_1.id
+
+  tags = {
+    Name = "nat-gateway"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.prod_vpc.id
+
+  tags = {
+    Name = "private-route-table"
+  }
+}
+
+resource "aws_route" "private_internet_access" {
+  route_table_id         = aws_route_table.private_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
+
+resource "aws_route_table_association" "private_subnet_1_assoc" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_subnet_2_assoc" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_rt.id
+}
